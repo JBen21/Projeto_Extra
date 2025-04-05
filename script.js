0382c1a62abe4fc98715bb7903325fa14830639e
Jessé Benício <cdcssloj@gmail.com>
2025-04-05 18:50:02 -0300

    git inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+/*8. Soma. Crie um script que receba três números e exiba a soma de todos.
+var valor1Soma = prompt("Informe o 1° Valor p/ Somar");
+var valor2Soma = prompt("Informe o 2° Valor p/ Somar");
+var valor3Soma = prompt("Informe o 3° Valor p/ Somar");
+    console.log (parseInt(valor1Soma) + parseInt(valor2Soma) + parseInt(valor3Soma));
+*/
+var valor1Media = prompt("Informe o 1° Valor p/ verificar a media");
+var valor2Media = prompt("Informe o 2° Valor p/ verificar a media");
+var valor3Media = prompt("Informe o 3° Valor p/ verificar a media");
+    console.log((parseInt(valor1Media) + parseInt(valor1Media) + parseInt(valor1Media)) / 3);
